62655319d1500549fc9e7b97ea53392534bd6a92
Alex Erastov <123257559+erastov-alex@users.noreply.github.com>
2023-09-12 19:18:44 +0300

    Update options.py

--- src/options.py
+++ src/options.py
@@ -67,8 +67,8 @@ CLASSES_PREVIEW_GAP: int = None
 ### * Set up visualization params for ClassesHeatmaps class ###
 # args for "to_image" method
 DRAW_STYLE: str = None  # "inside_white" or "outside_black"
-HEATMAP_ROWS: int = None
-HEATMAP_COLS: int = None
+HEATMAP_ROWS: int = 5
+HEATMAP_COLS: int = 5
 HEATMAP_GRID_SPACING: int = None
 HEATMAP_OUTER_GRID_SPACING: int = None
 HEATMAP_OUTPUT_WIDTH: int = (
