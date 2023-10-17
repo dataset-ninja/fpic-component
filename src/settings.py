90e3825a72c62a5e03e28cf7226e604ce405b858
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-17 14:51:44 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -22,7 +22,7 @@ HIDE_DATASET = False  # set False when 100% sure about repo quality
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = License.CC_BY_NC_ND_4_0()
+LICENSE: License = License.CC_BY_NC_ND_4_0(source_url="https://github.com/CandleLabAI/PCBSegClassNet#license")
 APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.WasteRecycling()]
 CATEGORY: Category = Category.Environmental(extra=Category.Manufacturing())
 
